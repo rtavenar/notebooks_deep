bf9028c727d9f634498e2f6b0d43a03e1e0b40a7
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2026-08-10 17:54:04 +0200

    tp5

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -2,7 +2,7 @@ pip install jupyter nbconvert torch torchvision scikit-learn matplotlib transfor
 
 mkdir -p executed
 exit_status=0
-for f in tp_seance01*_corr.ipynb tp_seance02*_corr.ipynb tp_seance03*_corr.ipynb; do
+for f in tp_seance04*_corr.ipynb; do
     echo "=== $f ==="
     if jupyter nbconvert --to notebook --execute \
         --ExecutePreprocessor.timeout=3600 \
